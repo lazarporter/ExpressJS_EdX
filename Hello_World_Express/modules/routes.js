b445984438ffde1acd0b47717d650a9633c5698c
Lazer Porter <lazar.porter@gmail.com>
2018-11-18 16:25:37 -0500

    tracking routes and server files

--- Hello_World_Express/modules/routes.js
+++ Hello_World_Express/modules/routes.js
@@ -15,3 +15,5 @@ module.exports = {
         res.send(profile)
     }*/
 }
+
+//https://courses.edx.org/courses/course-v1:Microsoft+DEV283x+3T2018/courseware/1e95019f-b0fe-1ae9-fcf4-4e35d66aa371/b841a5c1-d845-7d49-568c-a2ee63560e0d/?child=first
--- Hello_World_Express/server.js
+++ Hello_World_Express/server.js
@@ -14,6 +14,7 @@ let profile = {
 
 app.use((req, res, next)=>{
     console.log(`${req.method}: ${req.url}`)
+    
     next()
 })
 app.use(auth.checkAuth)       //run the request through the module/middleware called "auth", which checks for the precense of an API key.
